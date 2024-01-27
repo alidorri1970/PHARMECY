c3e2830fe4a579f9a092eeefae11159b8a6889d7
Salid19700 <alidori36@gmail.com>
2024-01-27 12:44:18 +0330

    props added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import { Product } from "./product";
 import "./style.css";
 
-function Header(props) {
+function Header() {
   return (
     <>
       <section className="header">
@@ -20,18 +20,45 @@ function Header(props) {
               <li>Store</li>
               <li>About</li>
               <li>
-                <span>in order({props.count})</span>
-                <span><img src="https://icon-icons.com/downloadimage.php?id=181878&root=2869/PNG/32/&file=order_confirmation_icon_181878.png" alt="" /></span>
+                <span>in order({})</span>
+                <span>
+                  <img
+                    src="https://icon-icons.com/downloadimage.php?id=181878&root=2869/PNG/32/&file=order_confirmation_icon_181878.png"
+                    alt=""
+                  />
+                </span>
               </li>
             </ul>
           </nav>
         </div>
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          photoP="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLUlJbPh0IS8P2wfc7Jxln60M6mpsHIdh9Kke6fBf9v3ZTmhA-QM609IY4jKvHn3yF6EQ&usqp=CAU"
+          titleP="S24 Ultra 5G"
+          desP="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt explicabo molestiae maiores tempore facilis corrupti eveniet necessitatibus adipisci nihil illo voluptate fuga accusamus reiciendis, debitis ullam deleniti alias dolorum id?"
+          logoP="https://storage3.torob.com/backend-api/internet_shop/logos/13dea769fd4c.png"
+          sellerP="ekala"
+          priceP="2400000"
+        />
+        <Product
+          photoP="https://assets.swappie.com/cdn-cgi/image/width=600,height=600,dpr=2,fit=contain,format=auto/swappie-iphone-14-pro-deep-purple.png?v=42"
+          titleP="iPhone 14 Pro"
+          desP="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt explicabo molestiae maiores tempore facilis corrupti eveniet necessitatibus adipisci nihil illo voluptate fuga accusamus reiciendis, debitis ullam deleniti alias dolorum id?"
+          logoP="https://storage3.torob.com/backend-api/internet_shop/logos/aaca0b83ebf6.png"
+          sellerP="ekala"
+          priceP="2640000"
+        />
+                <Product
+          photoP="https://i02.appmifile.com/274_operatorx_operatorx_opx/01/09/2023/935bb43c65b07d96c13ed1a03e95ad2a!348x348.png"
+          titleP="Xiaomi Redmi 12 4G"
+          desP="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt explicabo molestiae maiores tempore facilis corrupti eveniet necessitatibus adipisci nihil illo voluptate fuga accusamus reiciendis, debitis ullam deleniti alias dolorum id?"
+          logoP="https://storage3.torob.com/backend-api/internet_shop/logos/85ef7ed7fd49.png"
+          sellerP="ekala"
+          priceP="650000"
+        />
       </section>
     </>
   );
 }
 
 export default Header;
+
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -4,33 +4,33 @@ export function Product(props) {
   var [count, setCount] = useState(0);
   return (
     <>
+    
       <div className="container">
         <img
           className="photo-product"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLUlJbPh0IS8P2wfc7Jxln60M6mpsHIdh9Kke6fBf9v3ZTmhA-QM609IY4jKvHn3yF6EQ&usqp=CAU"
+          src={props.photoP}
           alt=""
         />
-        <h1 className="title-product">S24Ultra5G</h1>
+        <h1 className="title-product">{props.titleP}</h1>
         <p className="des-product">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore a
-          quidem illum saepe vero at natus reprehenderit?
-          natus.
+          {props.desP}
         </p>
         <img
           className="seller-logo"
-          src="https://storage3.torob.com/backend-api/internet_shop/logos/13dea769fd4c.png"
+          src={props.logoP}
           alt=""
         />
-        <h3 className="seller-product">Ekala{ifCluse(count)}</h3>
-        <h2 className="price-product">2120000</h2>
+        <h3 className="seller-product">{props.sellerP}:</h3>
+        <h2 className="price-product">{props.priceP}</h2>
+        <h3 className="counter-number">in order: {ifCluse(count)}</h3>
         <button onClick={()=> setCount (count + 1)} className="increasement">+</button>
         <button onClick={()=> setCount (count - 1)} className="decreasement">-</button>
       </div>
     </>
   );
   function ifCluse(count){
-    if(count < 0){
-      return "=>empty List";
+    if(count < 1){
+      return ":empty";
     }else{
       return count;
     }
